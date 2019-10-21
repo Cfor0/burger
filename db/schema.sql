e26cbe9674b63eff31c2229007c7126b921a2b81
Carlo Ortega <cfor0@outlook.com>
2019-10-21 14:11:56 -0400

    creating connection to sql and orm

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE burgers_db;
 CREATE TABLE burgers(
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(50),
-    devoured BOOLEAN FALSE,
+    devoured BOOLEAN DEFAULT FALSE,
     PRIMARY KEY (id)
 );
 
