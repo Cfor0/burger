a2b32a98aecd9958b9d30203bdbbf2c71e6a24e0
Carlo Ortega <cfor0@outlook.com>
2019-10-21 15:50:13 -0400

    wrote three orm functions for sql

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,29 @@
 var connection = require("./connection.js");
+
+
+var orm = {
+    //selectAll()
+    selectAll: function(tableName) {
+        var queryString = "SELECT * FROM ??";
+        connection.query(queryString, [tableName], function(err, result){
+            if (err) throw err;
+            console.log(result)
+        });
+    },
+    //insertOne()
+    insertOne: function(colName) {
+        var queryString = "INSERT INTO ??";
+        connection.query(queryString, [colName], function(err, result){
+            if (err) throw err;
+            console.log(result)
+        });
+    },
+    //updateOne()
+    updateOne: function(tableName, newData, colName ) {
+        var queryString = "UPDATE ?? SET ?? WHERE ??";
+        connection.query(queryString, [tableName, newData, colName], function(err, result){
+            if (err) throw err;
+            console.log(result);
+        })
+    }
+}
