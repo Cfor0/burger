96a564b4b1634c9805c7efa8e6cb24fa22abd3c4
Carlo Ortega <cfor0@outlook.com>
2019-10-28 18:14:17 -0400

    jaws

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,16 @@
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Passw0rd',
+    database:'burgers_db'
+  });
+};
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
