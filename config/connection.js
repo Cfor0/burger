3aaee47cd0587a19f044e717178cef1405f32ca7
Carlo Ortega <cfor0@outlook.com>
2019-11-10 16:06:50 -0500

    changing for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,18 @@
 var mysql = require("mysql");
-
+var connection;
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+ connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Passw0rd',
-    database:'burgers_db'
+    host: "z37udk8g6jiaqcbx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    user: "zusjsarwgqf032ks",
+    password: "nn42ph14e6m6tiul",
+    database: "rrnrhr9qqc5n4cr5"
   });
-};
+}
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Passw0rd",
-  database: "burgers_db"
-});
-
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
